4ba4e3efe3d0e9e6f73f74a8bdddb2e82c25a8ef
Julia Zhdanova <tratata7.89@mail.ru>
2022-06-18 22:30:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int costoftheticket = 20000;
+        int onemile = 20;
+        int totalmiles = costoftheticket / onemile;
+        System.out.println(costoftheticket / onemile);
+    }
+}
